a2cf8382478faa4f831e70c953b16293c5f7ebe2
ashrafkvt <ashrafkvt6197@gmail.com>
2022-03-20 15:41:25 +0530

    initial setup

--- dictionaryApp/dictApp/views.py
+++ dictionaryApp/dictApp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+
+import requests
+from bs4 import BeautifulSoup
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'dictApp/home.html')
