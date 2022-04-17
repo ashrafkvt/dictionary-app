b72e4e7faadb9747a5168b8ada393f8ac4e4fa00
ashrafkvt <ashrafkvt6197@gmail.com>
2022-04-17 15:07:09 +0530

    scrapping is done

--- dictionaryApp/dictApp/views.py
+++ dictionaryApp/dictApp/views.py
@@ -8,4 +8,24 @@ from bs4 import BeautifulSoup
 
 
 def home(request):
-    return render(request, 'dictApp/home.html')
+    if request.method ==  "POST":
+        word = request.POST['word']
+        print(word)
+        word_url = 'https://www.dictionary.com/browse/'+word
+        req = requests.get(word_url)
+        response_content = req.content
+        soup = BeautifulSoup(response_content, 'html.parser')
+        span = soup.find_all("span", {"class":"one-click-content"})
+        data = {}
+        data['word'] = word
+        if len(word) == 0:
+            data['meaning'] = "Please enter a word to search"
+        else:
+            if len(span) > 0:
+                data['meaning'] = span[0].text
+            else:
+                data['meaning'] = "please recheck what you've entered"
+        return render(request, 'dictApp/home.html', data)
+    else:
+        print("sdbsjdbsd")
+        return render(request, 'dictApp/home.html')
